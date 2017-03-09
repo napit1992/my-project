3e66667d4a329c4eb3f15200fd41a6e0109de1d2
napit1992 <napitdiken@gmail.com>
2017-03-09 15:41:37 -0600

    bhayo jasto cha

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+//var mongoose=require("mongoose");
+//mongoose.connect("mongodb://admin:admin@ds123050.mlab.com:23050/meeta");
+app.use(express.static("public"));
+
+
+app.set("view engine","ejs");
+const port = process.env.port ||3000;
+
+app.get("/",function(req,res){
+		res.sendfile("./public/dii.html");
+})
+
+
+
+app.listen(3000)
